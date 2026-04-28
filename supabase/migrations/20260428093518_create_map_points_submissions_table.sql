33f7e51fd276881cb41183ce01ff595b9c8bd0ac
Tony Vanin <vaninanton@gmail.com>
2026-04-28 15:01:19 +0500

    feat: enhance map points and routes tables with new constraints and indexing
    - Updated `map_points` and `map_routes` tables to use `GENERATED BY DEFAULT AS IDENTITY` for primary keys.
    - Added constraints for title and coordinates validation in both tables.
    - Introduced `submission_status` type and updated `map_points_submissions` table with new constraints and indexing for better data integrity and performance.
    - Created indexes on `flag_disabled` in both `map_points` and `map_routes` for optimized querying.
    
    These changes improve data validation and enhance the performance of queries related to map points and routes.

--- supabase/migrations/20260428093518_create_map_points_submissions_table.sql
+++ supabase/migrations/20260428093518_create_map_points_submissions_table.sql
@@ -1,15 +1,26 @@
+CREATE TYPE submission_status AS ENUM (
+    'pending',
+    'approved',
+    'rejected'
+);
+COMMENT ON TYPE submission_status IS 'Статус модерации пользовательской заявки';
+
 CREATE TABLE map_points_submissions (
-    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     processed_at timestamp with time zone,
-    type text NOT NULL,
+    type point_types NOT NULL,
     title text NOT NULL,
     coordinates jsonb NOT NULL,
     flag_is_meeting boolean DEFAULT false NOT NULL,
-    status text DEFAULT 'pending'::text NOT NULL
-    CONSTRAINT map_points_submissions_status_check CHECK ((status = ANY (ARRAY['pending'::text, 'approved'::text, 'rejected'::text]))),
+    status submission_status DEFAULT 'pending'::submission_status NOT NULL,
     CONSTRAINT map_points_submissions_title_check CHECK ((char_length(TRIM(BOTH FROM title)) > 0)),
-    CONSTRAINT map_points_submissions_type_check CHECK ((type = ANY (ARRAY['point'::text, 'socket'::text])))
+    CONSTRAINT map_points_submissions_coordinates_check CHECK (
+        (jsonb_typeof(coordinates) = 'array'::text)
+        AND (jsonb_array_length(coordinates) = 2)
+        AND ((coordinates ->> 0)::double precision BETWEEN -180::double precision AND 180::double precision)
+        AND ((coordinates ->> 1)::double precision BETWEEN -90::double precision AND 90::double precision)
+    )
 );
 
 COMMENT ON TABLE map_points_submissions IS 'Пользовательские заявки на добавление точек и розеток.';
@@ -18,3 +29,5 @@ COMMENT ON COLUMN map_points_submissions.coordinates IS 'Координаты [l
 CREATE POLICY insert_map_points_submissions ON map_points_submissions FOR INSERT TO anon WITH CHECK (true);
 
 ALTER TABLE map_points_submissions ENABLE ROW LEVEL SECURITY;
+
+CREATE INDEX map_points_submissions_status_created_at_idx ON map_points_submissions (status, created_at DESC);
